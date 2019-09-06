10bcf89883bfafbae4c26243a2618be957c17d67
Justin Nguyen <justnguyen1@gmail.com>
2019-09-06 10:50:37 -0700

    fourth commit

--- app/src/main/java/com/example/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.java
@@ -2,23 +2,24 @@ package com.example.passwordgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView result;
+
     Button generate;
+    Button btn_copy;
 
-
-
-
-
-
+    ClipboardManager clipboardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView) findViewById(R.id.result);
 
         generate = (Button) findViewById(R.id.generate);
+
+        btn_copy = findViewById(R.id.btn_copy);
+
+
+
+        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        btn_copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = result.getText().toString();
+
+                if (!text.equals("")) {
+                    ClipData clipData = ClipData.newPlainText("text", text);
+                    clipboardManager.setPrimaryClip(clipData);
+
+                    Toast.makeText(MainActivity.this, "Copied",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         generate.setOnClickListener(new View.OnClickListener() {
             @Override
